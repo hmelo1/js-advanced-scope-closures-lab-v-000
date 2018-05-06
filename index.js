eec49650848a59f417f7fae31973e26499139c24
Eddie Melo <heriberto@melo.nyc>
2018-05-06 16:48:52 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,16 @@
+function produceDrivingRange(blockRange){
+  return function(startRange, endRange){
+    let requestRange = parseInt(endRange) - parseInt(startRange);
+    if (requestRange > blockRange){
+      return `${requestRange - blockRange} blocks out of range`;
+    } else {
+      return `within range by ${requestRange}`;
+    }
+  }
+}
+
+function produceTipCalculator(percentage){
+  return function(tip){
+    return tip*percentage;
+  }
+}
