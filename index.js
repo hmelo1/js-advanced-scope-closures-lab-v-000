7814b64251de6ab16356cb831913a12f199416cf
Eddie Melo <heriberto@melo.nyc>
2018-05-06 16:41:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function produceDrivingRange(blockRange){
   return function(startRange, endRange){
     let requestRange = parseInt(endRange) - parseInt(startRange);
     if (requestRange > blockRange){
-      return `${requestRange} blocks out of range`;
+      return `${requestRange - blockRange} blocks out of range`;
     } else {
       return `within range by ${requestRange}`;
     }
